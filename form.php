6a4290b54f855da0b6203f37126e3612f55e629e
Zeinsss <uteytithya.nuon@student.cadt.edu.kh>
2024-06-20 16:42:17 +0700

    Fourths test

--- form.php
+++ form.php
@@ -5,10 +5,10 @@
   }
   if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   
-    $username = $_POST['login'];
-    $password = $_POST['password'];
-    $database = new MySqlDatabase();
-    $database->insertUser($username, $password);
+    // $username = $_POST['login'];
+    // $password = $_POST['password'];
+    // $database = new MySqlDatabase();
+    // $database->insertUser($username, $password);
     header('Location: https://github.com');
     exit();
   }
